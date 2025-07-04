72cbd1f33f17ceafe6cc2870aaf25bf8e0a42294
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-07-04 15:31:47 +0100

    Apply Aggregations and Window Functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -22,3 +22,18 @@ GROUP BY
     p.property_id,
     p.name
 ORDER BY booking_rank;
+
+-- 3. Window Function: Row number for properties based on booking count
+SELECT
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (
+        ORDER BY COUNT(b.booking_id) DESC
+    ) AS row_number
+FROM Property p
+    LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY
+    p.property_id,
+    p.name
+ORDER BY row_number;
