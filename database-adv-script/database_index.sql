cd675ce1f3339d0b4a740e370482ae68f6bff8b6
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2025-07-04 15:35:04 +0100

    Implement Indexes for Optimization

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,5 +1,3 @@
--- Indexes to improve performance on frequently queried columns
-
 -- USER TABLE INDEXES
 CREATE INDEX idx_user_email ON User (email);
 
@@ -16,3 +14,30 @@ CREATE INDEX idx_booking_user_id ON Booking (user_id);
 CREATE INDEX idx_booking_property_id ON Booking (property_id);
 
 CREATE INDEX idx_booking_dates ON Booking (start_date, end_date);
+
+-- EXPLAIN ANALYZE: Check if indexes are used
+
+-- 1. Check if index on user_id in Booking is used
+EXPLAIN ANALYZE
+SELECT *
+FROM Booking
+WHERE
+    user_id = '00000000-0000-0000-0000-000000000001';
+
+-- 2. Check if composite index on (start_date, end_date) is used
+EXPLAIN ANALYZE
+SELECT *
+FROM Booking
+WHERE
+    start_date >= '2024-07-01'
+    AND end_date <= '2024-07-31';
+
+-- 3. Check if host_id index in Property table is used
+EXPLAIN ANALYZE
+SELECT *
+FROM Property
+WHERE
+    host_id = '00000000-0000-0000-0000-000000000011';
+
+-- 4. Check if email index in User is used
+EXPLAIN ANALYZE SELECT * FROM User WHERE email = 'host@example.com';
